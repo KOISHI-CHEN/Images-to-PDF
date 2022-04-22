298b755715ad1cd196e348fae09fd08bf11b91e7
KOISHI-CHEN <75569629+KOISHI-CHEN@users.noreply.github.com>
2022-04-22 19:23:04 +0800

    Fix issue 1044 App crashes while adding big size images

--- app/src/main/java/swati4star/createpdf/util/ImageUtils.java
+++ app/src/main/java/swati4star/createpdf/util/ImageUtils.java
@@ -311,7 +311,6 @@ public class ImageUtils {
      */
     public int calculateInSampleSize(
             BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
         final int height = options.outHeight;
         final int width = options.outWidth;
         int inSampleSize = 1;
@@ -320,8 +319,6 @@ public class ImageUtils {
             final int halfHeight = height / 2;
             final int halfWidth = width / 2;
 
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
             while ((halfHeight / inSampleSize) >= reqHeight
                     && (halfWidth / inSampleSize) >= reqWidth) {
                 inSampleSize *= 2;
